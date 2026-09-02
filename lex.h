96fe50bd482d5518fc9bd3c8b4cf0347b5ad56cf
Silly Cat <CatC0der@proton.me>
2026-09-01 22:00:01 -0700

    token added

--- lex.h
+++ lex.h
@@ -2,5 +2,24 @@
 #define CATSCRIPT_LEX_H
 #include <stddef.h>
 
+enum tok_t{
+	INT_LITTERAL,
+	FLOAT_LITTERAL,
+	STRING_LITTERAL,
+	IDENTIFIER,
+	LINE_END,
+	FILE_END,
+};
+union tok_t{
+	int int_litteral;
+	float float_litteral;
+	char * string_litteral;
+	char * identifier;
+};
+struct tok{
+	enum tok_t type;
+	union tok_t data;
+};
+
 void lex(const char *source_code, size_t len);
 #endif // CATSCRIPT_LEX_H
